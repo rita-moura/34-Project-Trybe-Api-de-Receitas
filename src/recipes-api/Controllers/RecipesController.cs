624329058bd3b46020c568883712d7bb4aa7061a
Rita Moura <ritamoura520@gmail.com>
2023-07-17 19:45:23 -0300

    Feat: Implementa o verbo get(name) em /recipes.

--- src/recipes-api/Controllers/RecipesController.cs
+++ src/recipes-api/Controllers/RecipesController.cs
@@ -38,7 +38,12 @@ public class RecipesController : ControllerBase
     [HttpGet("{name}", Name = "GetRecipe")]
     public IActionResult Get(string name)
     {                
-        throw new NotImplementedException();
+        var recipeByName = this._service.GetRecipe(name);
+        if(recipeByName == null)
+        {
+            return NotFound();
+        }
+        return Ok(recipeByName);
     }
 
     // 3 - Sua aplicação deve ter o endpoint POST /recipe
